734b547e865968f9737d3632ab3cfd762ff0b6c3
Christian Mills <millscj@protonmail.com>
2023-07-06 19:01:25 -0700

    Update build_model function.

--- cjm_yolox_pytorch/model.py
+++ cjm_yolox_pytorch/model.py
@@ -935,7 +935,7 @@ def build_model(model_type:str, # Type of the model to be built.
     backbone = CSPDarknet(**backbone_cfg)
     neck = YOLOXPAFPN(**neck_cfg)
 
-    if PRETRAINED_URLS[model_type] == None:
+    if pretrained and PRETRAINED_URLS[model_type] == None:
         print("The selected model type does not have a pretrained checkpoint. Initializing model with untrained weights.")
         pretrained = False
     
