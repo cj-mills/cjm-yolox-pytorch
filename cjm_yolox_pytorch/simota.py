ec01e3d2973fc0778e35c5f42ac0bebb69712345
Christian Mills <millscj@protonmail.com>
2023-07-25 14:42:48 -0700

    Update loss functions.

--- cjm_yolox_pytorch/simota.py
+++ cjm_yolox_pytorch/simota.py
@@ -107,8 +107,8 @@ class SimOTAAssigner():
         gt_onehot_label = F.one_hot(gt_labels.to(torch.int64), pred_scores.shape[-1]).float().unsqueeze(0).repeat(num_valid, 1, 1)
         valid_pred_scores = valid_pred_scores.unsqueeze(1).repeat(1, num_gt, 1)
             
-        valid_pred_scores = torch.sigmoid(valid_pred_scores)
-        cls_cost = F.binary_cross_entropy(valid_pred_scores, gt_onehot_label, reduction='none').sum(-1)
+#         valid_pred_scores = torch.sigmoid(valid_pred_scores)
+        cls_cost = F.binary_cross_entropy_with_logits(valid_pred_scores, gt_onehot_label, reduction='none').sum(-1)
 #         cls_cost = F.binary_cross_entropy(valid_pred_scores.sqrt_(), gt_onehot_label, reduction='none').sum(-1)
         
         # Calculate total cost matrix by combining classification and IoU costs, 
